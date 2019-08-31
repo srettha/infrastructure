cb33878eaf992c92e9bb820a520054f53d5788b5
Srettha Terananont <strayedpeople@gmail.com>
2019-08-31 15:55:16 +0700

    remove access_key and secret_key file, by grabbing it from profile in awscli

--- terraform/digitalocean/prod/main.tf
+++ terraform/digitalocean/prod/main.tf
@@ -1,11 +1,9 @@
 terraform {
   backend "s3" {
-    access_key                  = "___CHANGE_ME___"
     bucket                      = "terraform-state-bucket"
     endpoint                    = "https://sgp1.digitaloceanspaces.com"
     key                         = "prod/terraform.tfstate"
     region                      = "ap-southeast-1"
-    secret_key                  = "___CHANGE_ME___"
     skip_credentials_validation = true
     skip_get_ec2_platforms      = true
     skip_requesting_account_id  = true
@@ -15,7 +13,7 @@ terraform {
 
 module "default-droplet-prod" {
   source                = "../modules/default-app"
-  
+
   count                 = 2
   name                  = "default-app-sgp1-prod"
   pub_key               = "${file("./files/digitalocean-prod.pub")}"
--- terraform/digitalocean/staging/main.tf
+++ terraform/digitalocean/staging/main.tf
@@ -1,11 +1,9 @@
 terraform {
   backend "s3" {
-    access_key                  = "___CHANGE_ME___"
     bucket                      = "terraform-state-bucket"
-    endpoint                    = "https://sgp1.digitaloceanspaces.com"
+    endpoint                    = "sgp1.digitaloceanspaces.com"
     key                         = "staging/terraform.tfstate"
     region                      = "ap-southeast-1"
-    secret_key                  = "___CHANGE_ME___"
     skip_credentials_validation = true
     skip_get_ec2_platforms      = true
     skip_requesting_account_id  = true
@@ -15,7 +13,7 @@ terraform {
 
 module "default-droplet-staging" {
   source                = "../modules/default-app"
-  
+
   count                 = 1
   name                  = "default-app-sgp1-staging"
   pub_key               = "${file("./files/digitalocean-staging.pub")}"
